ca94ef3ce9a864aa063dc483bfc434564a679dbe
Peter Jones <pjones@devalot.com>
2018-04-08 10:40:55 -0700

    Update to latest nix-hs generated default.nix file

--- default.nix
+++ default.nix
@@ -1,17 +1,2 @@
-# These arguments are so you can override settings from the command
-# line using the `nix-hs' tool.
-{ nixpkgs   ? import <nixpkgs> { }
-, compiler  ? "default"
-, profiling ? false
-}:
-
-let
-  pkgs = nixpkgs;
-
-  buildInputs = with pkgs; [
-    # List extra dependencies here.
-  ];
-
-in
-  pkgs.nix-hs.interactive ./personal-webhooks.nix
-    { inherit compiler profiling buildInputs; }
+{ pkgs ? (import <nixpkgs> {}).pkgs }:
+pkgs.haskellPackages.callPackage ./personal-webhooks.nix { }
