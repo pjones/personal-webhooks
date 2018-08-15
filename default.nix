54fac42202200a7d00e404a3236516bd0de56c80
Peter Jones <pjones@devalot.com>
2018-08-15 16:42:50 -0700

    Add a build script that can be built with Nix without nixpkgs

--- default.nix
+++ default.nix
@@ -1,2 +1,10 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
+let
+  # If no package source is specified then default to this URL:
+  commit  = "5b8a24a40ce11bbe1cb6ebf33ddb5adaaebbd43a";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs-channels/tarball/${commit}";
+in
+
+{ pkgs ? (import nixpkgs {}).pkgs
+}:
+
 pkgs.haskellPackages.callPackage ./personal-webhooks.nix { }
