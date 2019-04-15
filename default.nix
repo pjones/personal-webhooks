184a50e74caa47071f7df02343e6a9a59d0cbb58
Peter Jones <pjones@devalot.com>
2019-04-15 15:05:40 -0700

    Update dependencies for NixOS 19.03

--- default.nix
+++ default.nix
@@ -1,21 +1,20 @@
+{ pkgs ? (import <nixpkgs> {})
+}:
+
 let
-  # If no package source is specified then default to this URL:
-  commit  = "5b8a24a40ce11bbe1cb6ebf33ddb5adaaebbd43a";
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs-channels/tarball/${commit}";
+  # Remove the broken flag:
+  unBreak = drv: pkgs.haskell.lib.overrideCabal drv (drv: { broken = false; });
 
   # Helper function to override Haskell packages:
-  haskellOverride = pkgs:
+  haskellOverride =
     pkgs.haskellPackages.override (orig: {
       overrides = pkgs.lib.composeExtensions
                     (orig.overrides or (_: _: {}))
                     (self: super: with pkgs.haskell.lib; {
-                      table-layout = dontCheck (doJailbreak super.table-layout);
+                      table-layout = unBreak (dontCheck (doJailbreak super.table-layout));
                     });
       });
 
 in
 
-{ pkgs ? (import nixpkgs {}).pkgs
-}:
-
-(haskellOverride pkgs).callPackage ./personal-webhooks.nix { }
+haskellOverride.callPackage ./personal-webhooks.nix { }
